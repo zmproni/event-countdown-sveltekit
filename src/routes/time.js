6067e8ae5525f7be983da245792717c6df3fd19d
Alessandro Joshua Zamproni <zmproni@gmail.com>
2023-02-02 13:56:00 +0000

    Temporary timezone

--- src/routes/time.js
+++ src/routes/time.js
@@ -1,13 +1,12 @@
 import { readable } from "svelte/store";
 import { DateTime } from 'luxon'
 
-export const current = readable(DateTime.now(), function start(set) {
+export const current = readable(DateTime.now().setZone('UTC+8'), function start(set) {
     const interval = setInterval(() => {
-        set(DateTime.now());
+        set(DateTime.now().setZone('UTC+8'));
     }, 1000);
     
     return function stop() {
         clearInterval(interval);
     };
 });
-
